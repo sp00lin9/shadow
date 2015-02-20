d60ee37ce60d2b288b96521bc8f799d9ad80320b
SDCDev <sdcoin@sdcoin.co>
2015-02-20 12:08:15 +0200

    Blocks to maturity change in local wallet

--- src/main.cpp
+++ src/main.cpp
@@ -3241,7 +3241,7 @@ int CMerkleTx::GetBlocksToMaturity() const
     if (!(IsCoinBase() || IsCoinStake()))
         return 0;
 
-    return max(0, (nCoinbaseMaturity) - GetDepthInMainChain());
+    return max(0, (nCoinbaseMaturity + 5) - GetDepthInMainChain());
 }
 
 
